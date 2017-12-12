ec4264de33f698300932c762ebd8f70cb577db00
Esther Yaeeun Jeon <32020849+estheryjeon@users.noreply.github.com>
2017-12-12 05:46:21 -0500

    Update profile.js

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,23 +1,68 @@
 $(document).ready(function() {
 
-  $.ajax({
-    type: "GET",
-    url: '/userName',
-    success: function(email) {
-      console.log(email);
-      $.ajax({
-        type: "GET",
-        url: '/profile/' + email,
-        success: function(data) {
-          var username = data[0].username;
-          $("#userName").append(username);
-          var age = data[0].age;
-          $("#age").append(age);
-          var location = data[0].location;
-          $("#location").append(location);
-        }
-      });
+  var newName = '';
+  var newAge = '';
+  var newLocation = '';
+  var newBio = '';
+
+  var getUserData = function() {
+    $.ajax({
+      type: "GET",
+      url: '/getProfile',
+      success: function(data) {
+        var username = data.username;
+        $("#userName").append(username);
+        var age = data.age;
+        $("#age").append('<p>Age : ' + age + '</p>');
+        var location = data.location;
+        $("#location").append('<p>Location : ' + location + '</p>');
+        $("#bio").append('<p>About Me : ' + data.bio + '</p>');
+      }
+    });
+  }
+
+  var updateProfile = function() {
+    $.ajax({
+      type: "POST",
+      url: "/updateProfile",
+      data: {
+        name: newName,
+        age: newAge,
+        location: newLocation,
+        bio: newBio
+      },
+    });
+  }
+
+  getUserData();
+
+  $('#update').click(function () {
+    $('#userInfo').hide();
+    $('#userUpdate').show();
+  });
+
+  $("#done").click(function() {
+    $('#userUpdate').hide();
+    if ($('#updateName').val()) {
+      newName = $('#updateName').val();
     }
+    if ($('#updateAge').val()) {
+      newAge = $('#updateAge').val();
+    }
+    if ($('#updateLocation').val()) {
+      newLocation = $('#updateLocation').val();
+    }
+    if ($('#updateBio').val()) {
+      newBio = $('#updateBio').val();
+    }
+    updateProfile();
+
+    $("#userName").empty();
+    $('#age').empty();
+    $("#location").empty();
+    $("#bio").empty();
+    getUserData();
+    $('#userInfo').show();
   });
 
 });
